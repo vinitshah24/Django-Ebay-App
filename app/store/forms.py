0f2d6828a0ec5b3a916cd1a8e42097c8411ec976
Vinit Shah <shahvinit2244@gmail.com>
2020-04-14 00:15:35 -0400

    search form

--- app/store/forms.py
+++ app/store/forms.py
@@ -8,3 +8,42 @@ class SearchProducts(forms.Form):
         required=True,
         widget=forms.TextInput(attrs={'class': "form-control"})
     )
+    product_id = forms.CharField(
+        max_length=50,
+        required=False,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Product ID',
+            'class': "form-control border-right-0 border"
+        })
+    )
+    product_count = forms.IntegerField(
+        required=False,
+        initial=1,
+        min_value=1,
+        max_value=100,
+        label='Count',
+        widget=forms.NumberInput(attrs={'class': "form-control"})
+    )
+    start_date = forms.DateField(
+        required=False,
+        label='Auction Start Date',
+        widget=forms.SelectDateWidget(
+            years=[y for y in range(2000, 2021)],
+            attrs={'class': "form-control mb-1"}
+        )
+    )
+    end_date = forms.DateField(
+        required=False,
+        label='Auction End Date',
+        widget=forms.SelectDateWidget(
+            years=[y for y in range(2000, 2021)],
+            attrs={'class': "form-control mb-1"}
+        )
+    )
+    category = forms.CharField(
+        max_length=50,
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': "form-control"
+        })
+    )
--- app/store/views.py
+++ app/store/views.py
@@ -3,12 +3,23 @@ from django.contrib.auth.decorators import login_required
 from . import utils
 from . import forms
 
+
 @login_required
 def store(request):
     if request.method == 'POST':
         form = forms.SearchProducts(request.POST)
         if form.is_valid():
             search_query = form.cleaned_data.get('search_query')
+            product_id = form.cleaned_data.get('product_id')
+            product_count = form.cleaned_data.get('product_count')
+            start_date = form.cleaned_data.get('start_date')
+            category = form.cleaned_data.get('category')
+
+            print(product_id)
+            print(str(product_count))
+            print(start_date)  # 2000-04-01
+            print(category)
+
             raw_data = utils.api_query(search_query)
             products = utils.get_data(raw_data)
             context = {'products': products, 'form': form}
